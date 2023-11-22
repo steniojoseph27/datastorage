bf39d83f94af45b6140f7923b02ca3e3990b43ff
Joseph Stenio <joseph.stenio@franklinenergy.com>
2023-11-22 00:24:44 -0400

    Added UnitTest Project, Create User Test Removed HomeController

---
+++ DataStorage/DataStorageTest/TestUser.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using Moq;
+using DataAccessLayer;
+using BusinessLogicLayer;
+using BusinessLogicLayer.Services;
+using DataAccessLayer.Repositories;
+using DataAccessLayer.Entities;
+
+namespace DataStorageTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Mock<UserRepository> _userRepositoryMock;
+        private UserService _userService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userRepositoryMock = new Mock<UserRepository>();
+            _userService = new UserService(_userRepositoryMock.Object);
+        }
+
+        [Test]
+        public async Task CreateUserAsync_ShouldCallAddUserOnce()
+        {
+            var user = new User
+            {
+                FirstName = "Stenio",
+                LastName = "Joseph",
+                Age = 33,
+                DateOfBirth = DateTime.Now,
+                AddressID = 1
+            };
+
+            await _userService.CreateUserAsync(user);
+
+            _userRepositoryMock.Verify(repo => repo.AddUserAsync(It.IsAny<User>()), Times.Once);
+        }
+    }
+}
